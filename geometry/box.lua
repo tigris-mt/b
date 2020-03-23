9b48fb66f8d6b2fcd6f78c159300f9a4bd01be73
Beha <shacknetisp@mail.com>
2020-03-23 10:38:20 -0400

    don't clobber b

--- geometry/box.lua
+++ geometry/box.lua
@@ -1,10 +1,11 @@
-b.box = {}
+local m = {}
+b.box = m
 
 -- Returns true if box a and box b have collided.
-function b.box.collide_box(a, b)
+function m.collide_box(a, b)
     local e = {
-        a = b.box.extremes(a),
-        b = b.box.extremes(b),
+        a = m.extremes(a),
+        b = m.extremes(b),
     }
 
     local function beyond(axis)
@@ -30,10 +31,10 @@ function b.box.collide_box(a, b)
 end
 
 -- Returns true if <inner> is inside <outer>.
-function b.box.inside_box(inner, outer)
+function m.inside_box(inner, outer)
 	local e = {
-        i = b.box.extremes(inner),
-        o = b.box.extremes(outer),
+        i = m.extremes(inner),
+        o = m.extremes(outer),
     }
 
 	for _,axis in ipairs({"x", "y", "z"}) do
@@ -46,51 +47,51 @@ function b.box.inside_box(inner, outer)
 end
 
 -- Check if <point> collides with <box>.
-function b.box.collide_point(box, point)
-	return b.box.collide_box(box, b.box.new(point, point))
+function m.collide_point(box, point)
+	return m.collide_box(box, m.new(point, point))
 end
 
 -- Get the extremes of the box.
-function b.box.extremes(box)
+function m.extremes(box)
 	local min, max = vector.sort(box.a, box.b)
-	return b.box.new(min, max)
+	return m.new(min, max)
 end
 
 -- Get the box translated to a position
-function b.box.translate(box, pos)
-	return b.box.new(vector.add(box.a, pos), vector.add(box.b, pos))
+function m.translate(box, pos)
+	return m.new(vector.add(box.a, pos), vector.add(box.b, pos))
 end
 
 -- From corners.
-function b.box.new(a, b)
+function m.new(a, b)
 	return {a = a, b = b}
 end
 
 -- From addition.
-function b.box.new_add(a, b)
-	return b.box.new(a, vector.add(a, b))
+function m.new_add(a, b)
+	return m.new(a, vector.add(a, b))
 end
 
 -- From entity collision box.
-function b.box.new_cbox(box)
+function m.new_cbox(box)
     return b.geometry.Box.new(vector.new(box[1], box[2], box[3]), vector.new(box[4], box[5], box[6]))
 end
 
 -- From radius.
-function b.box.new_radius(center, radius)
-	return b.box.new(vector.subtract(center, radius), vector.add(center, radius))
+function m.new_radius(center, radius)
+	return m.new(vector.subtract(center, radius), vector.add(center, radius))
 end
 
 -- Convert box to VoxelArea.
-function b.box.voxelarea(box)
+function m.voxelarea(box)
 	return VoxelArea:new{MinEdge=box.a, MaxEdge=box.b}
 end
 
 -- Get all positions in the box.
 -- If sort_near is specified, the positions will be sorted in order nearest to sort_near.
-function b.box.poses(box, sort_near)
+function m.poses(box, sort_near)
 	local poses = {}
-	local box = b.box.extremes(box)
+	local box = m.extremes(box)
 	for x=box.a.x,box.b.x do
 		for y=box.a.y,box.b.y do
 			for z=box.a.z,box.b.z do
