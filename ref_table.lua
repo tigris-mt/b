fdd31e8de7ea38f1cfa3b576e3136864d9a9cd53
Beha <shacknetisp@mail.com>
2020-04-14 10:43:25 -0400

    Add b.ref_to_table

---
+++ ref_table.lua
@@ -0,0 +1,8 @@
+-- Convert object reference to serializable table.
+-- Extend in mods implementing mobs/etc.
+-- Returns nil if unable to convert.
+function b.ref_to_table(obj)
+	if obj:is_player() then
+		return {type = "player", id = obj:get_player_name()}
+	end
+end
